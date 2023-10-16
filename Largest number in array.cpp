c073c2ecdb9f666f58014b0e71cffc8e122ee522
YarraChaitanyakiran <124281663+YarraChaitanyakiran@users.noreply.github.com>
2023-10-16 09:35:53 +0530

    Add files via upload

---
+++ Largest number in array.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int i,n,arr[i];
+	printf("Enter the number of elements: ");
+	scanf("%d",arr[i]);
+	for(int i=0;i<n;++i);
+	if(arr[0]<arr[i])
+	arr[0]=arr[i];
+	printf("largest array element=%.2f",arr[0]);
+	return 0;
+}
